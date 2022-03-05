5562e9bd6da988cd8ad9337bb3300894630b6ab8
naveengoud <naveen8185922062@gmail.com>
2022-03-05 11:16:43 +0530

    first commit

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
     // Write your code here
+    var dict ={}
+    for (x=0 ; x<arr.length ; x=x+2){
+        dict[arr[x]] = arr[x+1]
+    }
+    return dict
 }
 
 module.exports = CreateObject;
